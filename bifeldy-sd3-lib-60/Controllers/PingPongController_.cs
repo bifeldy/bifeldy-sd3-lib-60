c2c32d00b5f2518c467d57eb8ae3c777ce8ebe23
B. Bias A. Ch <bias@indomaret.co.id>
2024-04-18 17:08:31 +0700

    Test Ping - Pong !!

---
+++ bifeldy-sd3-lib-60/Controllers/PingPongController_.cs
@@ -0,0 +1,36 @@
+﻿/**
+ * 
+ * Author       :: Basilius Bias Astho Christyono
+ * Phone        :: (+62) 889 236 6466
+ * 
+ * Department   :: IT SD 03
+ * Mail         :: bias@indomaret.co.id
+ * 
+ * Catatan      :: Default Ping Pong API Endpoint
+ *              :: Tidak Untuk Didaftarkan Ke DI Container
+ * 
+ */
+
+using Microsoft.AspNetCore.Mvc;
+
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace bifeldy_sd3_lib_60.Controllers {
+
+    [ApiController]
+    [Route("ping-pong")]
+    public class PingPongController : ControllerBase {
+
+        public PingPongController() {
+            //
+        }
+
+        [HttpHead]
+        [SwaggerOperation(Summary = "Untuk test Ping-Pong saja")]
+        public IActionResult PingPong() {
+            return Ok();
+        }
+
+    }
+
+}
--- bifeldy-sd3-lib-60/Models/ListApiDc_.cs
+++ bifeldy-sd3-lib-60/Models/ListApiDc_.cs
@@ -19,6 +19,7 @@ namespace bifeldy_sd3_lib_60.Models {
         public string APP_NAME { get; set; }
         public string API_PATH { get; set; }
         public string DEFAULT_API_PATH { get; set; }
+        public decimal? PING_PONG { get; set; }
     }
 
 }
