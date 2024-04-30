31ea533c97304a1134e1f10c80e42f26b1419934
B. Bias A. Ch <bias@indomaret.co.id>
2024-04-30 16:01:35 +0700

    Refactor ~

---
+++ bifeldy-sd3-lib-60/AttributeFilterDecorators/SwaggerIgnore_.cs
@@ -0,0 +1,20 @@
+﻿/**
+ * 
+ * Author       :: Basilius Bias Astho Christyono
+ * Phone        :: (+62) 889 236 6466
+ * 
+ * Department   :: IT SD 03
+ * Mail         :: bias@indomaret.co.id
+ * 
+ * Catatan      :: Filter Terima Content Type
+ *              :: Tidak Untuk Didaftarkan Ke DI Container
+ * 
+ */
+
+namespace bifeldy_sd3_lib_60.AttributeFilterDecorators {
+
+    public class SwaggerIgnoreAttribute : Attribute {
+        //
+    }
+
+}
---
+++ bifeldy-sd3-lib-60/AttributeFilterDecorators/SwaggerSkipPropertyFilter_.cs
@@ -0,0 +1,42 @@
+﻿/**
+ * 
+ * Author       :: Basilius Bias Astho Christyono
+ * Phone        :: (+62) 889 236 6466
+ * 
+ * Department   :: IT SD 03
+ * Mail         :: bias@indomaret.co.id
+ * 
+ * Catatan      :: Filter Terima Content Type
+ *              :: Tidak Untuk Didaftarkan Ke DI Container
+ * 
+ */
+
+using System.Reflection;
+
+using Microsoft.OpenApi.Models;
+
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace bifeldy_sd3_lib_60.AttributeFilterDecorators {
+
+    public sealed class SwaggerSkipPropertyFilter : ISchemaFilter {
+
+        public void Apply(OpenApiSchema schema, SchemaFilterContext context) {
+            if (schema?.Properties == null) {
+                return;
+            }
+
+            var skipProperties = context.Type.GetProperties().Where(t => t.GetCustomAttribute<SwaggerIgnoreAttribute>() != null);
+
+            foreach (var skipProperty in skipProperties) {
+                var propertyToSkip = schema.Properties.Keys.SingleOrDefault(x => string.Equals(x, skipProperty.Name, StringComparison.OrdinalIgnoreCase));
+
+                if (propertyToSkip != null) {
+                    schema.Properties.Remove(propertyToSkip);
+                }
+            }
+        }
+
+    }
+
+}
