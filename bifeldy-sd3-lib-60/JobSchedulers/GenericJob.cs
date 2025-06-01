ab7dab15d0b39ad69bb04c74d3e17062aa80758d
B. Bias A. Ch <bias@indomaret.co.id>
2025-06-01 14:49:03 +0700

    Refactor ~

--- bifeldy-sd3-lib-60/JobSchedulers/GenericJob.cs
+++ bifeldy-sd3-lib-60/JobSchedulers/GenericJob.cs
@@ -33,49 +33,53 @@ namespace bifeldy_sd3_lib_60.JobSchedulers {
 
         public async Task Execute(IJobExecutionContext context) {
             foreach (KeyValuePair<string, object> jdm in context.JobDetail.JobDataMap) {
-                string errorMessage = null;
-
-                _ = await this._orapg.ExecQueryAsync(
-                    $@"
-                        DELETE FROM api_quartz_job_queue
-                        WHERE job_name = :job_name
-                    ",
-                    new List<CDbQueryParamBind>() {
-                        new() { NAME = "job_name", VALUE = jdm.Key }
-                    }
-                );
-
                 try {
-                    var func = (Func<IJobExecutionContext, IServiceProvider, Task>)jdm.Value;
+                    string errorMessage = null;
 
                     _ = await this._orapg.ExecQueryAsync(
                         $@"
-                            INSERT INTO api_quartz_job_queue (job_name, start_at, completed_at, error_message)
-                            VALUES (:job_name, CURRENT_TIMESTAMP, NULL, NULL)
+                            DELETE FROM api_quartz_job_queue
+                            WHERE job_name = :job_name
                         ",
                         new List<CDbQueryParamBind>() {
                             new() { NAME = "job_name", VALUE = jdm.Key }
                         }
                     );
 
-                    await func(context, this._serviceProvider);
-                }
-                catch (Exception ex) {
-                    errorMessage = ex.Message;
-                    this._logger.LogError("[{Name}_ERROR] ⌚ {Message}", $"{this.GetType().Name}_{jdm.Key}", errorMessage);
-                }
+                    try {
+                        var func = (Func<IJobExecutionContext, IServiceProvider, Task>)jdm.Value;
 
-                _ = await this._orapg.ExecQueryAsync(
-                    $@"
-                        UPDATE api_quartz_job_queue
-                        SET error_message = :error_message {(string.IsNullOrEmpty(errorMessage) ? ", completed_at = CURRENT_TIMESTAMP" : "")}
-                        WHERE job_name = :job_name
-                    ",
-                    new List<CDbQueryParamBind>() {
-                        new() { NAME = "error_message", VALUE = errorMessage },
-                        new() { NAME = "job_name", VALUE = jdm.Key }
+                        _ = await this._orapg.ExecQueryAsync(
+                            $@"
+                                INSERT INTO api_quartz_job_queue (job_name, start_at, completed_at, error_message)
+                                VALUES (:job_name, CURRENT_TIMESTAMP, NULL, NULL)
+                            ",
+                            new List<CDbQueryParamBind>() {
+                                new() { NAME = "job_name", VALUE = jdm.Key }
+                            }
+                        );
+
+                        await func(context, this._serviceProvider);
                     }
-                );
+                    catch (Exception ex) {
+                        errorMessage = ex.Message;
+                    }
+
+                    _ = await this._orapg.ExecQueryAsync(
+                        $@"
+                            UPDATE api_quartz_job_queue
+                            SET error_message = :error_message {(string.IsNullOrEmpty(errorMessage) ? ", completed_at = CURRENT_TIMESTAMP" : "")}
+                            WHERE job_name = :job_name
+                        ",
+                        new List<CDbQueryParamBind>() {
+                            new() { NAME = "error_message", VALUE = errorMessage },
+                            new() { NAME = "job_name", VALUE = jdm.Key }
+                        }
+                    );
+                }
+                catch (Exception e) {
+                    this._logger.LogError("[{Name}_ERROR] ⌚ {Message}", $"{this.GetType().Name}_{jdm.Key}", e.Message);
+                }
             }
         }
 
